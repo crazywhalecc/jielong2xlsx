a0d78c25b157e7be5e47b95a94cba95ed9a62e7a
Jerry Ma <jesse2061@outlook.com>
2022-05-03 09:59:05 +0800

    Update conver.php

--- html/conver.php
+++ html/conver.php
@@ -6,7 +6,7 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-if (!isset($_POST['filecontent'])) {
+if (!isset($_POST['filecontent'], $_POST['area_type'])) {
     echo '
 <html>
 <head>
@@ -16,6 +16,10 @@ if (!isset($_POST['filecontent'])) {
 <body>
 <form enctype="multipart/form-data" action="conver.php" method="post">
 <textarea rows="10" cols="20" name="filecontent" type="text"></textarea>
+<select name="area_type">
+  <option value="qi">期</option>
+  <option value="nong">弄</option>
+</select>
 <input type="submit" value="提交"/>
 </form>
 <pre>
@@ -45,31 +49,37 @@ if (!isset($_POST['filecontent'])) {
     exit();
 }
 $file = $_POST['filecontent'];
+$type = ['qi' => '期', 'nong' => '弄'];
+
 function filterEmoji($str)
 
 {
 
     $str = preg_replace_callback(
 
-            '/./u',
+        '/./u',
 
-            function (array $match) {
+        function (array $match) {
 
-                return strlen($match[0]) >= 4 ? '' : $match[0];
+            return strlen($match[0]) >= 4 ? '' : $match[0];
+        },
 
-            },
+        $str
+    );
 
-            $str);
-
-     return $str;
-
+    return $str;
 }
 $file = filterEmoji($file);
 //$file = file_get_contents("jielong.txt");
 $file = str_replace("\r\n", "\n", $file);
 $lines = explode("\n", $file);
-global $result, $ls, $false, $exception;
+global $result, $ls, $false, $exception, $type;
 global $argv;
+if (!isset($type[$_POST['area_type']])) {
+    die('INVALID area_type INPUT: ' . $_POST['area_type']);
+}
+
+$type = $type[$_POST['area_type']];
 array_shift($argv);
 $result = [];
 $ls = [];
@@ -86,6 +96,8 @@ resolve_no_qi();
 
 ksort($result);
 
+
+
 $spreadsheet = new Spreadsheet();
 $worksheet = $spreadsheet->getActiveSheet();
 $errorsheet = new Worksheet($spreadsheet, '有问题的');
@@ -94,13 +106,13 @@ $errorsheet = new Worksheet($spreadsheet, '有问题的');
 $worksheet->setTitle('统计表');
 
 $worksheet->setCellValueByColumnAndRow(1, 1, '序号');
-$worksheet->setCellValueByColumnAndRow(2, 1, '期');
+$worksheet->setCellValueByColumnAndRow(2, 1, $type);
 $worksheet->setCellValueByColumnAndRow(3, 1, '楼栋');
 $worksheet->setCellValueByColumnAndRow(4, 1, '室号');
 $worksheet->setCellValueByColumnAndRow(5, 1, '附加信息');
 $n = 6;
 foreach ($argv as $v) {
-    $worksheet->setCellValueByColumnAndRow($n, 1, '物品【'.$v.'】');
+    $worksheet->setCellValueByColumnAndRow($n, 1, '物品【' . $v . '】');
     ++$n;
 }
 
@@ -108,7 +120,7 @@ $errorsheet->setCellValueByColumnAndRow(1, 1, '接龙序号');
 $errorsheet->setCellValueByColumnAndRow(2, 1, '内容');
 
 $ni = 2;
-foreach($result as $k => $v) {
+foreach ($result as $k => $v) {
     $worksheet->setCellValueByColumnAndRow(1, $ni, $k);
     $worksheet->setCellValueByColumnAndRow(2, $ni, $v[0]);
     $worksheet->setCellValueByColumnAndRow(3, $ni, $v[1]);
@@ -126,7 +138,7 @@ foreach($result as $k => $v) {
     ++$ni;
 }
 $ni = 2;
-foreach($exception as $k => $v) {
+foreach ($exception as $k => $v) {
     $errorsheet->setCellValueByColumnAndRow(1, $ni, $k);
     $errorsheet->setCellValueByColumnAndRow(2, $ni, $v);
     ++$ni;
@@ -144,13 +156,13 @@ $styleArrayBody = [
 ];
 $total_rows = count($result) + 2;
 //添加所有边框/居中
-$worksheet->getStyle('A1:E'.$total_rows)->applyFromArray($styleArrayBody);
+$worksheet->getStyle('A1:E' . $total_rows)->applyFromArray($styleArrayBody);
 $worksheet->getColumnDimension('B')->setAutoSize(true);
 $worksheet->getColumnDimension('E')->setAutoSize(true);
 $errorsheet->getColumnDimension('B')->setAutoSize(true);
 $spreadsheet->addSheet($errorsheet, 1);
 header('Content-Type: application/vnd.ms-excel'); // 设置文件类型
-header('Content-Disposition: attachment;filename="'.'output.xlsx"'); // 设置文件名称及文件后缀，文件名可使用变量替代。
+header('Content-Disposition: attachment;filename="' . 'output.xlsx"'); // 设置文件名称及文件后缀，文件名可使用变量替代。
 header('Cache-Control: max-age=0'); // 设置缓存时间，此处为0不缓存
 $writer = new Xlsx($spreadsheet);
 
@@ -163,36 +175,36 @@ function full_trim($txt)
     return trim($txt, "- \n\t，、,。");
 }
 
-function compile_full_text($content, $num) 
+function compile_full_text($content, $num)
 {
-    global $result, $ls, $false;
+    global $result, $ls, $false, $type;
     $patterns = [
-        "*期*号楼*室*",
-        "*期*号楼*",
-        "*期*楼*室*",
-        "*期*楼*",
-        "*期*号*室*",
-        "*期*号*",
-        "*期*栋*室*",
-        "*期*栋*",
-        "*期-*-*",
-        "*期*-*",
+        "*$type*号楼*室*",
+        "*$type*号楼*",
+        "*$type*楼*室*",
+        "*$type*楼*",
+        "*$type*号*室*",
+        "*$type*号*",
+        "*$type*栋*室*",
+        "*$type*栋*",
+        "*$type-*-*",
+        "*$type*-*",
     ];
     foreach ($patterns as $id => $pattern) {
         if (($arg = match_args($pattern, $content)) !== false) {
-            if (count($arg) < 3) die(json_encode($arg, 128|256));
-            $r = match_room($arg, in_array($id, [0,2,4,6]), "match[$id]:".implode(",", $arg)."");
-            if (in_array($id, [0,2,4,6])) {
+            if (count($arg) < 3) die(json_encode($arg, 128 | 256));
+            $r = match_room($arg, in_array($id, [0, 2, 4, 6]), "match[$id]:" . implode(",", $arg) . "");
+            if (in_array($id, [0, 2, 4, 6])) {
                 $content = $arg[3];
             } else {
                 $content = str_replace($r[2], '', $arg[2]);
             }
-            $content = str_replace(["，","、","；"], " ", full_trim($content));
-            $r[]=$content;
+            $content = str_replace(["，", "、", "；"], " ", full_trim($content));
+            $r[] = $content;
             $r = array_merge($r, explode_items($content));
             $result[$num] = $r;
             //echo $num.PHP_EOL;
-            $ls[$num]=$arg;
+            $ls[$num] = $arg;
             break;
         }
     }
@@ -205,9 +217,9 @@ function explode_items($content)
 {
     global $argv;
     $q = [];
-    foreach($argv as $v) {
+    foreach ($argv as $v) {
         if (($pos = mb_strpos($content, $v)) !== false) {
-            $q[$v] = get_start_num(full_trim(mb_substr($content, $pos + mb_strlen($v))), 'Match:'.$content.",$v");
+            $q[$v] = get_start_num(full_trim(mb_substr($content, $pos + mb_strlen($v))), 'Match:' . $content . ",$v");
         }
     }
     return [$q];
@@ -216,7 +228,7 @@ function explode_items($content)
 function match_room($arg, $end_room = false, $content = '')
 {
     $qi = get_end_num($arg[0], $content);
-    if ($qi !== 1 && $qi !== 2) die('出错啦：'.json_encode($arg, 128|256));
+    //if ($qi !== 1 && $qi !== 2) die('出错啦：' . json_encode($arg, 128 | 256));
     $hao = get_end_num($arg[1], $content);
     $room = $end_room ? get_end_num($arg[2], $content) : get_start_num($arg[2], $content);
     return [$qi, $hao, $room];
@@ -224,7 +236,7 @@ function match_room($arg, $end_room = false, $content = '')
 
 function resolve_no_qi()
 {
-    global $result, $false, $exception;
+    global $result, $false, $exception, $type;
     foreach ($false as $k => $v) {
         $patterns = [
             "*号楼*室*",
@@ -240,9 +252,12 @@ function resolve_no_qi()
         foreach ($patterns as $id => $pattern) {
             if (($arg = match_args($pattern, $v)) !== false) {
                 $hao = get_end_num($arg[0], $v);
-                array_unshift($arg, $hao >= 25 ? '2' : '1');
-                $r = match_room($arg, in_array($id, [0,2,4,6]));
-                if (in_array($id, [0,2,4,6])) {
+                if ($type === 'qi')
+                    array_unshift($arg, $hao >= 25 ? '2' : '1');
+                else
+                    array_unshift($arg, '0');
+                $r = match_room($arg, in_array($id, [0, 2, 4, 6]));
+                if (in_array($id, [0, 2, 4, 6])) {
                     $content = mb_substr($arg[3], mb_strpos($arg[3], "室"));
                 } else {
                     $content = str_replace($r[2], '', $arg[2]);
@@ -270,11 +285,11 @@ function get_end_num($str, $content = '')
         $n = $p . $n;
         --$i;
     }
-    if (!is_numeric($n)) die("数字解析错误end:".$str."\n$content");
+    if (!is_numeric($n)) die("数字解析错误end:" . $str . "\n$content");
     return (int) $n;
 }
 
-function get_start_num($str, $content = '') 
+function get_start_num($str, $content = '')
 {
     $str = trim($str,  "-# ，,号");
     $n = '';
@@ -283,21 +298,23 @@ function get_start_num($str, $content = '')
         $n .= strval($p);
         ++$i;
     }
-    if (!is_numeric($n)) die("数字解析错误start:".$str."\n$content");
+    if (!is_numeric($n)) die("数字解析错误start:" . $str . "\n$content");
     return (int) $n;
 }
 
 function replace_num($str): string
 {
-    $str = str_replace(['一','两','二','三','四','五','六','七','八','九','十'],[' 1',' 2',' 2',' 3',' 4',' 5',' 6',' 7',' 8',' 9'], $str);
-    $str = str_replace(['瓶','个','箱','盒','篮','斤','袋','幅','蓝'],'', $str);
-    for($i = 0; $i < mb_strlen($str); ++$i) {
-        if (mb_substr($str, $i, 1) === "套" && is_numeric(mb_substr($str, $i-1,1))) {
+    $str = str_replace(['一', '两', '二', '三', '四', '五', '六', '七', '八', '九', '十'], [' 1', ' 2', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9'], $str);
+    $str = str_replace(['瓶', '个', '箱', '盒', '篮', '斤', '袋', '幅', '蓝'], '', $str);
+    for ($i = 0; $i < mb_strlen($str); ++$i) {
+        if (mb_substr($str, $i, 1) === "套" && is_numeric(mb_substr($str, $i - 1, 1))) {
             $str = mb_substr($str, 0, $i) . mb_substr($str, $i + 1);
         }
     }
     $str = str_replace('—', '-', $str);
     $str = str_replace('–', '-', $str);
+    $str = str_replace('－', '-', $str);
+    $str = str_replace('～', '-', $str);
     $str = str_replace('#', '-', $str);
     return $str;
 }
